3e7ecf9a4f5246c75fa1523760e0f94a34fe7bbe
Jonathan Brink <jonathandavidbrink@gmail.com>
2022-10-11 19:14:28 -0400

    modernize

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,26 @@ import data, { RawItem } from "./data";
 import moment, { Moment, unitOfTime } from "moment";
 import "./App.css";
 
+interface Item extends RawItem {
+  formattedAge: string;
+  daysUntilBirthday: number;
+}
+
 // https://codepen.io/blackjacques/pen/RKPKba
 const getFormattedDateDiff = (
   date1: string | Moment,
-  date2: string | Moment,
-  intervals?: unitOfTime.Diff[]
+  date2: string | Moment
 ) => {
   var b = moment(date1),
     a = moment(date2),
     out: string[] = [];
-  intervals = intervals || ["years", "months"];
-  function singularize(s: string, num: number) {
-    return num === 1 ? s.slice(0, -1) : s;
-  }
-  intervals.forEach(function (interval) {
+  const singularize = (s: string, num: number) =>
+    num === 1 ? s.slice(0, -1) : s;
+  const intervals: unitOfTime.Diff[] = ["years", "months"];
+  intervals.forEach((interval) => {
     var diff = a.diff(b, interval);
     b.add(diff, interval);
-    if (diff > 0) {
-      out.push(diff + " " + singularize(interval, diff));
-    }
+    if (diff > 0) out.push(`${diff} ${singularize(interval, diff)}`);
   });
   return out.join(", ") || "0";
 };
@@ -31,140 +32,6 @@ const getDayOfYear = (m: string | moment.Moment) =>
 const nowMonth = new Date().getMonth() + 1;
 const nowDate = new Date().getDate();
 
-// top-level React control
-interface HomeProps {
-  dates: Item[];
-}
-const Home = ({ dates }: HomeProps) => {
-  const formattedNow = moment().format("MMMM DD, YYYY");
-  const topAnchor = React.createElement(
-    "a",
-    {
-      href: "./",
-    },
-    formattedNow
-  );
-
-  const createTimestamp = () => React.createElement("h3", null, topAnchor);
-
-  const createTable = (data: Item[]) => {
-    const createHeaderRow = () =>
-      React.createElement("tr", null, [
-        React.createElement("th", null, ""),
-        React.createElement("th", null, ""),
-        React.createElement("th", null, ""),
-        React.createElement("th", null, ""),
-      ]);
-    const createRows = () => {
-      return data.map(
-        ({ name, death, hide, formattedAge, birthday, daysUntilBirthday }) => {
-          let rowColor = "";
-          if (death) rowColor = "passedAway";
-          else if (hide === true) rowColor = "hide";
-          const [, month, date] = birthday.split("-");
-          if (
-            !death &&
-            nowMonth === parseInt(month) &&
-            nowDate === parseInt(date)
-          ) {
-            return React.createElement(
-              "tr",
-              {
-                className: rowColor ? `${rowColor} today` : "today",
-                style: {},
-              },
-              [
-                React.createElement(
-                  "td",
-                  { colSpan: 2 },
-                  formattedAge.replace(/[^\d]+/, "") + " " + name.toUpperCase()
-                ),
-                React.createElement(
-                  "td",
-                  { style: { fontSize: "16px" } },
-                  birthday
-                ),
-                React.createElement("td", null, ""),
-              ]
-            );
-          } else {
-            return React.createElement("tr", { className: rowColor }, [
-              React.createElement("td", null, name),
-              React.createElement(
-                "td",
-                { colSpan: death ? 3 : 1 },
-                death
-                  ? `${formattedAge.replace(
-                      /([\d]+).*$/,
-                      "$1"
-                    )} (${birthday} - ${death})`
-                  : formattedAge.replace(" months", "mo").replace(" years", "y")
-              ),
-              death ? null : React.createElement("td", null, birthday),
-              death
-                ? null
-                : React.createElement(
-                    "td",
-                    null,
-                    "(-" + daysUntilBirthday + ")"
-                  ),
-            ]);
-          }
-        }
-      );
-    };
-    return React.createElement(
-      "table",
-      {},
-      [createHeaderRow()].concat(createRows())
-    );
-  };
-
-  const numberOfUpcoming = 4;
-  const createUpcoming = () => {
-    let sorted = dates.slice().filter(({ hide }) => hide !== true);
-    sorted = sorted.sort(
-      (
-        { death: death0, daysUntilBirthday: daysUntilBirthday0 },
-        { daysUntilBirthday: daysUntilBirthday1 }
-      ) => {
-        if (death0) return 1;
-        else if (daysUntilBirthday0 === daysUntilBirthday1) return 0;
-        else if (daysUntilBirthday0 < daysUntilBirthday1) return -1;
-        else if (daysUntilBirthday0 > daysUntilBirthday1) return 1;
-        return 0;
-      }
-    );
-
-    const display = [];
-    for (let i = 0; i < numberOfUpcoming; i++) {
-      display.push(sorted[i].name + ": " + sorted[i].daysUntilBirthday);
-    }
-
-    const soonLabel = React.createElement("span", {}, "Soon: ");
-    return React.createElement(
-      "h4",
-      {
-        className: "soon",
-      },
-      [soonLabel, display.join(", ")]
-    );
-  };
-
-  return React.createElement(
-    "div",
-    {},
-    createTimestamp(),
-    createUpcoming(),
-    createTable(dates)
-  );
-};
-
-interface Item extends RawItem {
-  formattedAge: string;
-  daysUntilBirthday: number;
-}
-// called for each member of window.aData
 const transformData = (o: RawItem): Item => {
   const now = moment();
   const currentYear = new Date().getFullYear();
@@ -193,9 +60,103 @@ const transformData = (o: RawItem): Item => {
   };
 };
 
-function App() {
-  const dates = data.map(transformData);
-  return <Home dates={dates} />;
-}
+const dates = data.map(transformData);
+
+const numberOfUpcoming = 4;
+const createUpcoming = () => {
+  let sorted = dates.slice().filter(({ hide }) => !hide);
+  sorted = sorted.sort(
+    (
+      { death: death0, daysUntilBirthday: daysUntilBirthday0 },
+      { daysUntilBirthday: daysUntilBirthday1 }
+    ) => {
+      if (death0) return 1;
+      else if (daysUntilBirthday0 === daysUntilBirthday1) return 0;
+      else if (daysUntilBirthday0 < daysUntilBirthday1) return -1;
+      else if (daysUntilBirthday0 > daysUntilBirthday1) return 1;
+      return 0;
+    }
+  );
+  const display = [];
+  for (let i = 0; i < numberOfUpcoming; i++) {
+    display.push(`${sorted[i].name}: ${sorted[i].daysUntilBirthday}`);
+  }
+  return display.join(", ");
+};
+
+const App = () => (
+  <div>
+    <h3>
+      <a href="./">{moment().format("MMMM DD, YYYY")}</a>
+    </h3>
+    <h4 className="soon">
+      <span>Soon: </span>
+      {createUpcoming()}
+    </h4>
+    <table>
+      <tbody>
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+        {dates.map(
+          ({
+            name,
+            death,
+            hide,
+            formattedAge,
+            birthday,
+            daysUntilBirthday,
+          }) => {
+            let rowColor = "";
+            if (death) rowColor = "passedAway";
+            else if (hide === true) rowColor = "hide";
+            const [, month, date] = birthday.split("-");
+            return !death &&
+              nowMonth === parseInt(month) &&
+              nowDate === parseInt(date) ? (
+              <tr
+                className={rowColor ? `${rowColor} today` : "today"}
+                key={name}
+              >
+                <td colSpan={2}>
+                  {formattedAge.replace(/[^\d]+/, "") +
+                    " " +
+                    name.toUpperCase()}
+                </td>
+                <td
+                  style={{
+                    fontSize: "16px",
+                  }}
+                >
+                  {birthday}
+                </td>
+                <td></td>
+              </tr>
+            ) : (
+              <tr className={rowColor} key={name}>
+                <td>{name}</td>
+                <td colSpan={death ? 3 : 1}>
+                  {death
+                    ? `${formattedAge.replace(
+                        /([\d]+).*$/,
+                        "$1"
+                      )} (${birthday} - ${death})`
+                    : formattedAge
+                        .replace(" months", "mo")
+                        .replace(" years", "y")}
+                </td>
+                {death ? null : <td>{birthday}</td>}
+                {death ? null : <td>{`(-${daysUntilBirthday})`}</td>}
+              </tr>
+            );
+          }
+        )}
+      </tbody>
+    </table>
+  </div>
+);
 
 export default App;
