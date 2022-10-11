d5ca2b3a1a22ba452b8a84ed1fb2a3affd875953
Jonathan Brink <jonathandavidbrink@gmail.com>
2022-10-11 13:58:48 -0400

    content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import data from './data'
 
 function App() {
   return (
     <div className="App">
-      Ti
+    <table>
+    {data.map(({name, birthday}) => <tr><td>{name}</td><td>{birthday}</td></tr>)}
+</table>
     </div>
   )
 }
