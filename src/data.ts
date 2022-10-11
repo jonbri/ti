11fba34215d6fabf275fbfb416b2d22e67118ddc
Jonathan Brink <jonathandavidbrink@gmail.com>
2022-10-11 13:22:42 -0400

    ti

---
+++ src/data.ts
@@ -0,0 +1,182 @@
+const data = [
+  {
+    name: "Tracy",
+    birthday: "1983-07-07",
+  },
+  {
+    name: "Jonath",
+    birthday: "1985-08-12",
+  },
+  {
+    name: "Austin",
+    birthday: "2015-07-16",
+  },
+  {
+    name: "Jason",
+    birthday: "2017-05-08",
+  },
+  {
+    name: "Lisa",
+    birthday: "2019-11-14",
+  },
+  {
+    name: "LaoYe",
+    birthday: "1954-06-03",
+  },
+  {
+    name: "LaoLao",
+    birthday: "1955-02-07",
+  },
+  {
+    name: "Hadd",
+    birthday: "2019-06-28",
+  },
+  {
+    name: "Wedd",
+    birthday: "2014-01-31",
+  },
+  {
+    name: "School",
+    birthday: "2010-08-01",
+  },
+  {
+    name: "jonbri",
+    birthday: "2008-06-05",
+  },
+  {
+    name: "trsong",
+    birthday: "2020-03-03",
+  },
+  {
+    name: "NC",
+    birthday: "1996-08-01",
+  },
+  {
+    name: "AppleS",
+    birthday: "2021-07-02",
+  },
+  {
+    name: "Joe",
+    birthday: "1937-02-19",
+  },
+  {
+    name: "Phyllis",
+    birthday: "1941-09-19",
+    death: "2021-10-02",
+  },
+  {
+    name: "Lois",
+    birthday: "1936-04-03",
+    death: "1997-01-03",
+  },
+  {
+    name: "Gilbert",
+    birthday: "1929-05-26",
+    death: "2012-04-09",
+  },
+  {
+    name: "Alison",
+    birthday: "1991-09-13",
+  },
+  {
+    name: "Franc",
+    birthday: "1983-11-27",
+  },
+  {
+    name: "Oliver",
+    birthday: "2019-07-21",
+  },
+  {
+    name: "Ashley",
+    birthday: "1988-11-10",
+  },
+  {
+    name: "Alex",
+    birthday: "1986-06-17",
+  },
+  {
+    name: "Jacob",
+    birthday: "2012-03-07",
+  },
+  {
+    name: "Emma",
+    birthday: "2014-09-12",
+  },
+  {
+    name: "Sherry",
+    birthday: "1963-11-23",
+  },
+  {
+    name: "DB",
+    birthday: "1962-04-09"
+  },
+  {
+    name: "Michael",
+    birthday: "1961-08-31",
+  },
+  {
+    name: "Sebast",
+    birthday: "1963-04-27",
+  },
+  {
+    name: "Birgit",
+    birthday: "1963-02-27",
+  },
+  {
+    name: "Jenalee",
+    birthday: "1989-12-29",
+  },
+  {
+    name: "James",
+    birthday: "1982-06-12",
+  },
+  {
+    name: "JamesJR",
+    birthday: "2020-12-14",
+  },
+  {
+    name: "Steve",
+    birthday: "1958-02-04",
+  },
+  {
+    name: "Janet",
+    birthday: "1959-06-25",
+  },
+  {
+    name: "Matt",
+    birthday: "1991-02-14",
+  },
+  {
+    name: "David",
+    birthday: "1988-02-08",
+  },
+  {
+    name: "Adam",
+    birthday: "1985-04-28",
+  },
+  {
+    name: "Sarah",
+    birthday: "1981-03-14",
+  },
+  {
+    name: "Sol",
+    birthday: "1984-07-27",
+  },
+  {
+    name: "JA17",
+    birthday: "1996-05-21",
+  },
+  {
+    name: "Billy",
+    birthday: "2022-01-26",
+  },
+  {
+    name: "Boogie",
+    birthday: "2022-01-26",
+  },
+  {
+    name: "Respite",
+    birthday: "2022-08-01",
+  }
+];
+export default data;
